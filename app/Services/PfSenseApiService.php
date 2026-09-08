f219f8b57ad4436bd4fccd5ff4823aecb30b6e91
baga <baga@local>
2026-09-08 17:37:03 -0400

    feat(opnsense): integrate OpenVPN instances, status, and client/server APIs

--- app/Services/PfSenseApiService.php
+++ app/Services/PfSenseApiService.php
@@ -1405,6 +1405,9 @@ class PfSenseApiService
      */
     public function getOpenVpnServerStatus()
     {
+        if ($this->opnSense) {
+            return $this->opnSense->getOpenVpnServerStatus();
+        }
         return $this->get('/status/openvpn/servers');
     }
 
@@ -1487,6 +1490,9 @@ class PfSenseApiService
      */
     public function getOpenVpnServers()
     {
+        if ($this->opnSense) {
+            return $this->opnSense->getOpenVpnServers();
+        }
         return $this->get('/vpn/openvpn/servers');
     }
 
@@ -1495,6 +1501,9 @@ class PfSenseApiService
      */
     public function getOpenVpnClients()
     {
+        if ($this->opnSense) {
+            return $this->opnSense->getOpenVpnClients();
+        }
         return $this->get('/vpn/openvpn/clients');
     }
 
@@ -1517,9 +1526,23 @@ class PfSenseApiService
     /**
      * Delete OpenVPN Server
      */
-    public function deleteOpenVpnServer(int $id)
+    public function deleteOpenVpnServer($id)
     {
+        if ($this->opnSense) {
+            return $this->opnSense->deleteOpenVpnInstance((string) $id);
+        }
         return $this->delete("/vpn/openvpn/server", ['id' => $id]);
+    }
+
+    /**
+     * Delete OpenVPN Client
+     */
+    public function deleteOpenVpnClient($id)
+    {
+        if ($this->opnSense) {
+            return $this->opnSense->deleteOpenVpnInstance((string) $id);
+        }
+        return $this->delete("/vpn/openvpn/client", ['id' => $id]);
     }
 
     /**
@@ -1536,14 +1559,6 @@ class PfSenseApiService
     public function updateCarpStatus(array $data)
     {
         return $this->patch('/status/carp', $data);
-    }
-
-    /**
-     * Delete OpenVPN Client
-     */
-    public function deleteOpenVpnClient(int $id)
-    {
-        return $this->delete("/vpn/openvpn/client", ['id' => $id]);
     }
 
     /**
